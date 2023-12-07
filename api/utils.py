46cfb0decd671bfe3ef8d557a5a05b4f6e5a6db3
Roman <berdyshevrv@gmail.com>
2023-12-07 23:53:31 +0300

    update docstrings

--- api/utils.py
+++ api/utils.py
@@ -3,6 +3,16 @@ from dvc.repo import Repo
 
 
 def push_file_to_dvc(data: dict, data_name: str):
+    """
+
+    Загружает данные в репозиторий DVC (Data Version Control).
+
+    Аргументы:
+        data (dict): Словарь, содержащий данные, которые нужно загрузить в репозиторий DVC,
+                     где ключи представляют названия столбцов, и значения представляют сами данные для каждого столбца.
+        data_name (str): Название файла данных, которое будет создано в репозитории DVC.
+
+    """
 
     df = pd.DataFrame(data)
     repo = Repo()
